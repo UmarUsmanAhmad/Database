f83d85d5aa0720c14f86b6bdd8e8765ec61c4f0f
Umar Usman Ahmad <117400595+UmarUsmanAhmad@users.noreply.github.com>
2025-03-17 22:34:27 +0100

    Project.cs

--- Project.cs
+++ Project.cs
@@ -1,19 +1,17 @@
-
-using System;
-using System.ComponentModel.DataAnnotations;
-
 public class Project
 {
-    [Key]
     public int Id { get; set; }
-    
-    public string Number { get; set; } = string.Empty; 
-    public string Name { get; set; } = string.Empty;  
+    public string Name { get; set; } = string.Empty;
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public string Responsible { get; set; } = string.Empty;  
-    public string Customer { get; set; } = string.Empty; 
-    public string Service { get; set; } = string.Empty;  
+    public string Responsible { get; set; } = string.Empty;
+    
+    public int CustomerId { get; set; } 
+    public Customer? Customer { get; set; }  
+
+    public string Service { get; set; } = string.Empty;
     public decimal TotalPrice { get; set; }
-    public string Status { get; set; } = "Ej påbörjat";  
+    public string Status { get; set; } = string.Empty;
+    
+    public string ProjectNumber { get; set; } = string.Empty; 
 }
